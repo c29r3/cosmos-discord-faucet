56079ea51a8ffa7033ba33dd3085cbf234059eb0
Sergey <52304443+c29r3@users.noreply.github.com>
2021-11-22 00:41:15 +0300

    Fix for newer versions of Cosmos SDK

--- discord_faucet_bot.py
+++ discord_faucet_bot.py
@@ -10,6 +10,7 @@ import datetime
 import sys
 import cosmos_api as api
 
+print('Version 0.1.0')
 
 # Turn Down Discord Logging
 disc_log = logging.getLogger('discord')
@@ -21,13 +22,14 @@ logger = logging.getLogger(__name__)
 
 # Load config
 c = configparser.ConfigParser()
-c.read("config.ini")
+c.read("config.ini", encoding='utf-8')
 
 VERBOSE_MODE       = str(c["DEFAULT"]["verbose"])
 BECH32_HRP         = str(c["CHAIN"]["BECH32_HRP"])
 DECIMAL            = float(c["CHAIN"]["decimal"])
 DENOMINATION_LST   = c["TX"]["denomination_list"].split(",")
 AMOUNT_TO_SEND_LST = c["TX"]["amount_to_send"].split(",")
+# AMOUNT_TO_SEND_LST = [AMOUNT_TO_SEND_LST[0]] + [AMOUNT_TO_SEND_LST[1]] * 33
 FAUCET_SEED        = str(c["FAUCET"]["seed"])
 FAUCET_PRIVKEY     = str(c["FAUCET"]["private_key"])
 if FAUCET_PRIVKEY == "":
@@ -74,11 +76,11 @@ async def on_message(message):
 
     if message.content.startswith('$balance'):
         address = str(message.content).replace("$balance", "").replace(" ", "").lower()
-        if address[:len(BECH32_HRP)] == BECH32_HRP:
-            seq, acc_num, coins = await api.get_address_info(session, address)
-            if str(acc_num) != '0':
+        if str(address[:3]) == BECH32_HRP and len(address) == 42:
+            coins = await api.get_addr_balance(session, address)
+            if len(coins) >= 1:
                 await message.channel.send(f'{message.author.mention}\n'
-                                           f'```{api.coins_dict_to_string(coins, "grid")}```')
+                                           f'```{api.coins_dict_to_string(coins, headers="no")}```')
 
             else:
                 await message.channel.send(f'{message.author.mention} account is not initialized (balance is empty)')
@@ -91,8 +93,8 @@ async def on_message(message):
         print(requester.name, "status request")
         try:
             s = await api.get_node_status(session)
-            print(s)
-            seq, acc_num, coins = await api.get_address_info(session, FAUCET_ADDRESS)
+            coins = await api.get_addr_balance(session, FAUCET_ADDRESS)
+            seq, acc_num = await api.get_address_info(session, FAUCET_ADDRESS)
             if "node_info" in str(s) and "error" not in str(s):
                 s = f'```' \
                          f'Moniker:       {s["result"]["node_info"]["moniker"]}\n' \
@@ -100,7 +102,8 @@ async def on_message(message):
                          f'Syncs?:        {s["result"]["sync_info"]["catching_up"]}\n' \
                          f'Last block:    {s["result"]["sync_info"]["latest_block_height"]}\n' \
                          f'Voting power:  {s["result"]["validator_info"]["voting_power"]}\n' \
-                         f'Faucet balance:\n{api.coins_dict_to_string(coins, "")}```'
+                         f'Sequence:      {seq}\n' \
+                         f'Coins:\n{api.coins_dict_to_string(coins, headers="no")}```'
                 await message.channel.send(s)
 
         except Exception as statusErr:
@@ -117,18 +120,20 @@ async def on_message(message):
             hash_id = str(message.content).replace("$tx_info", "").replace(" ", "")
             if len(hash_id) == 64:
                 tx = await api.get_transaction_info(session, hash_id)
-                print(tx)
                 if "amount" and "fee" in str(tx):
                     from_   = tx["tx"]["value"]["msg"][0]["value"]["from_address"]
                     to_     = tx["tx"]["value"]["msg"][0]["value"]["to_address"]
-                    sended_coins = '\n'
+                    sended_coins = {}
                     for tx_ in tx["tx"]["value"]["msg"]:
-                        sended_coins = sended_coins + f'{tx_["value"]["amount"][0]["denom"]}: {tx_["value"]["amount"][0]["amount"]}\n'
+                        sended_coins.update({tx_["value"]["amount"][0]["denom"]: tx_["value"]["amount"][0]["amount"]})
 
+                    raw_log = tx["raw_log"]
                     tx = f'```' \
                          f'From:    {from_}\n' \
                          f'To:      {to_}\n' \
-                         f'Amount:  {sended_coins}```'
+                         f'{api.coins_dict_to_string(sended_coins, headers="no")}```\n' \
+                         f'raw_log: {raw_log[:150]}'
+
                     await message.channel.send(tx)
                 else:
                     await message.channel.send(f'{requester.mention}, `{tx}`')
@@ -143,9 +148,9 @@ async def on_message(message):
         channel = message.channel
         requester_address = str(message.content).replace("$request", "").replace(" ", "").lower()
 
-        if len(requester_address) != 44 or requester_address[:len(BECH32_HRP)] != BECH32_HRP:
+        if len(requester_address) != 42 or requester_address[:3] != BECH32_HRP:
             await channel.send(f'{requester.mention}, Invalid address format `{requester_address}`\n'
-                               f'Address length must be equal {len(FAUCET_ADDRESS)} and the suffix must be `{BECH32_HRP}`')
+                               f'Address length must be equal 42 and the suffix must be `{BECH32_HRP}`')
             return
 
         if requester.id in ACTIVE_REQUESTS:
@@ -162,21 +167,27 @@ async def on_message(message):
                 del ACTIVE_REQUESTS[requester.id]
 
         if requester.id not in ACTIVE_REQUESTS and requester_address not in ACTIVE_REQUESTS:
+
             ACTIVE_REQUESTS[requester.id] = {
                 "address": requester_address,
                 "requester": requester,
                 "next_request": message_timestamp + REQUEST_TIMEOUT}
             print(ACTIVE_REQUESTS)
 
+            coins = await api.get_addr_balance(session, FAUCET_ADDRESS)
+            seq, acc_num = await api.get_address_info(session, FAUCET_ADDRESS)
+            print(f'{coins=} {seq=} {acc_num=}')
+            coins = {i: coins[i] for i in coins if int(coins[i]) > int(AMOUNT_TO_SEND_LST[0])}
+
             transaction = await api.send_tx(session, recipient=requester_address,
-                                            denom_lst=DENOMINATION_LST, amount=AMOUNT_TO_SEND_LST)
+                                            denom_lst=list(coins.keys()),
+                                            amount=[AMOUNT_TO_SEND_LST[0]] * len(list(coins.keys())))
             logger.info(f'Transaction result:\n{transaction}')
-            with open('tx_log.csv', 'a') as csv_log:
-                csv_log.write(f'{requester.id};{requester.name};{requester_address};{message_timestamp}\n')
             print(transaction)
 
             if 'code' not in str(transaction) and "txhash" in str(transaction):
-                await channel.send(f'{requester.mention}, `$tx_info` {EXPLORER_URL}{transaction["txhash"]}\n')
+                await channel.send(f'{requester.mention}, `$tx_info {EXPLORER_URL}{transaction["txhash"]}\n`')
+
 
             else:
                 await channel.send(f'{requester.mention}, Can\'t send transaction. Try making another one request'
